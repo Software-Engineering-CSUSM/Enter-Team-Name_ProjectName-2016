884bc4b22d7351f7ec5b643ebe8c77645f0f35e0
stevenc <davolfman@gmail.com>
2016-09-21 11:51:24 -0700

    Removed old source folder

--- edu/csusm/TestTaker/QuestionRegistry.java
+++
@@ -1,8 +0,0 @@
-package edu.csusm.TestTaker;
-
-import java.io.Serializable;
-
-public class QuestionRegistry implements Serializable{
-	static final long serialVersionUID = 1L;
-	static java.util.HashMap<String, Question> questionStore;
-}
--- edu/csusm/TestTaker/TestRegistry.java
+++
@@ -1,8 +0,0 @@
-package edu.csusm.TestTaker;
-
-import java.io.Serializable;
-
-public class TestRegistry implements Serializable{
-	static final long serialVersionUID = 1L;
-	static java.util.HashMap<String, Test> testStore;
-}
