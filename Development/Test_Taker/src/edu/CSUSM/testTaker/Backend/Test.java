3b220cf75125f076de628e11dee5ecb2da405b00
Steven Clark <davolfman@gmail.com>
2016-09-25 13:46:31 -0700

    partial conversion of toString for Test, switching editors

--- Development/Test_Taker/src/edu/CSUSM/testTaker/Backend/Test.java
+++ Development/Test_Taker/src/edu/CSUSM/testTaker/Backend/Test.java
@@ -14,5 +14,24 @@ public class Test implements Serializable{
 	volatile ArrayList<Question> questionList;
 	ArrayList<String> questionIDs;
 	ArrayList<Integer> questionPoints;
+	String title;
 	
+	@Override
+    public String toString(){
+        
+        //Create the initial question
+        String thisTestString = "Test: " + this.title;
+                
+        //Now, add each of the possible answers in the provided question
+        for(int iterator = 0; iterator < questionList.Length() ; iterator++){
+            thisQuestionString += "\n\t\t " + (iterator+1) + ") " + this.questionList.    ;//finish this line
+            
+            //If the answer is correct, add an asterisk to the end
+            thisQuestionString += (iterator == this._correctIndex) ? " *correct*" : "";
+        }
+        
+        //Return the result
+        return thisQuestionString;
+    }
+
 }
