d3b51248e076f66e9bebd63312b703e09d0e51b4
John Orcino <orcin002@cougars.csusm.edu>
2016-09-26 18:21:12 -0700

    added a few functions into Test

--- Development/Test_Taker/src/edu/CSUSM/testTaker/Backend/QuestionResponse.java
+++ Development/Test_Taker/src/edu/CSUSM/testTaker/Backend/QuestionResponse.java
@@ -5,5 +5,10 @@ import java.io.Serializable;
 public class QuestionResponse implements Serializable{
 	static final long serialVersionUID = 1L;
 	String myID;
-
+/*	Author: John Orcino
+ * 
+ * 
+ * 
+ * 
+ */
 }
--- Development/Test_Taker/src/edu/CSUSM/testTaker/Backend/Test.java
+++ Development/Test_Taker/src/edu/CSUSM/testTaker/Backend/Test.java
@@ -15,4 +15,33 @@ public class Test implements Serializable{
 	ArrayList<String> questionIDs;
 	ArrayList<Integer> questionPoints;
 	
+	/*Author: John Orcino
+	 * PARAMETER: May need one
+	 * FUNCTION: gets the Id for the question to have the array of questions and answers
+	 */
+	public void getQuestionIDs(){
+		
+	}
+	
+	/*	Author: John Orcino
+	 * 	FUNCTION: adds up the total points of the test and returns it
+	 */
+	public int totalPointsScored(){
+		int tot = 0;
+		
+		//iterator adding up the question points 
+		
+		return tot;
+	}
+	
+	/*	Author: John Orcino
+	 * 	PARAMETERS:
+	 * 	FUNCTION: Display the question and answers, stores the user's answer
+	 * 
+	 */
+	
+	public void testDisplay(){
+		//iterator that uses the array of answers and questions and displays them
+	}
+	
 }
