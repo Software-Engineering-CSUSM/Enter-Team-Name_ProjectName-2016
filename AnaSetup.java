bc2a5ecdb92ccfd283534e3b82b0474ea8215153
Christian Mathias <cmmathias95@gmail.com>
2016-10-17 07:44:48 -0700

    Update AnaSetup.java

--- AnaSetup.java
+++ AnaSetup.java
@@ -1,20 +1,41 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class AnaSetup {
-	private static DateFormat dateReg = new SimpleDateFormat("MM-dd-yyyy | HH:mm:ss");
+	private static DateFormat dateReg = new SimpleDateFormat("dd-MM-yyyy|HH:mm:ss");
+	
+	//Presently unused
+	public static void bootUp() throws IOException{
 		
-	public static void sendEvent(String Action){
-		String st = assembleSt("Event");
-		
-		System.out.println(st + Action + "occured.");
 	}
 	
-	public static void sendPage(String pageName){
-		String st = assembleSt("Page");
+	//Send an event - unexpected jumps, errors, etc.
+	public static void sendEvent(String Action) throws IOException{
+		String st = assembleSt("Event");
+		System.out.println(st + Action + " occured.");
 		
-		System.out.println(st + pageName + "was accessed.");
+		//write to local.txt
+		FileWriter fw = new FileWriter("C:\\AnaManager\\Analytics.txt", true);
+		PrintWriter out = new PrintWriter(fw);
+		out.println(st + Action + " was accessed");
+		out.close();
+	}
+	
+	//Send a page access - main menu, test page, etc.
+	public static void sendPage(String pageName) throws IOException{
+		String st = assembleSt("Page ");
+		System.out.println(st + pageName + " was accessed.");
+		
+		//write to local.txt
+		FileWriter fw = new FileWriter("C:\\AnaManager\\Analytics.txt", true);
+		PrintWriter out = new PrintWriter(fw);
+		out.println(st + pageName + " was accessed");
+		out.close();
+			
 	}
 	
 	//Ex. 07-08-1995|12:12:30|Event:
@@ -24,6 +45,14 @@ public class AnaSetup {
 		String msg = dateReg.format(date) + "|" + category + ": "; 
 		return msg;
 	}
+	
+	//clear recorded data - may move to different class?
+	public static void clearData() throws IOException{
+		FileWriter fileWriter = new FileWriter("C:\\AnaManager\\Analytics.txt", false);
+		fileWriter.write(" ");
+		fileWriter.close();
+		
+		System.out.println("Data Cleared");
+	}
 }
-
 
