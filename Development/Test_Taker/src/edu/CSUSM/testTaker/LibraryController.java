062444057a35afd45ebc74e0f47cdf4074a643c8
Justin Goulet <jstngoulet@me.com>
2016-10-30 22:59:00 -0700

    added basic layout for table view. Still needs mroe testing, multiline support, etc

--- Development/Test_Taker/src/edu/CSUSM/testTaker/LibraryController.java
+++ Development/Test_Taker/src/edu/CSUSM/testTaker/LibraryController.java
@@ -1,6 +1,7 @@
 package edu.CSUSM.testTaker;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -57,6 +58,20 @@ public class LibraryController {
 	 */
 	public static Iterator<String> courseIDIterator(){
 		return classArray.keySet().iterator();
+	}
+	
+	/**
+	 * @return An array of all available courses
+	 */
+	public static ArrayList<Course> getAllCourses(){
+		//Create an array
+		ArrayList<Course> tempHolder = new ArrayList<Course>();
+		
+		//Add all classes to the array
+		tempHolder.addAll(classArray.values());
+		
+		//Return the list
+		return tempHolder;
 	}
 		
 	
