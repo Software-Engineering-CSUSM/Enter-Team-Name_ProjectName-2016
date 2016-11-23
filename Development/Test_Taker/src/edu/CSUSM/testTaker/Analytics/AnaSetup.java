0a53ed07ee4fb047d28145e6c5aa844f1b2a8bb1
Steven Clark <davolfman@gmail.com>
2016-11-23 13:40:37 -0800

    Fixed amd relocated anasetup again.

--- Development/Test_Taker/src/edu/CSUSM/testTaker/Analytics/AnaSetup.java
+++ Development/Test_Taker/src/edu/CSUSM/testTaker/Analytics/AnaSetup.java
@@ -1,5 +1,6 @@
 package edu.CSUSM.testTaker.Analytics;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -9,38 +10,75 @@ import java.util.Date;
 
 public class AnaSetup {
 	private static DateFormat dateReg = new SimpleDateFormat("MM_dd");
+	private static String logFileName = "Analytics.txt";
 	
-	
-	/**
-	* Log an event - outputs to "C:\\AnaManager\\Analytics.txt"
-	* Action is the name you want recorded in the log. This will be reflected in the graph if john finishes it.
-	 * @param Action
-	 * @throws IOException
-	 */
+	//Log an event - outputs to "C:\\AnaManager\\Analytics.txt"
+	//Action is the name you want recorded in the log. This will be reflected in the graph if john finishes it.
 	public static void logEvent(String Action) throws IOException{
 		Date date = new Date();
-		
+		File dirFile = null;
 		String msg = dateReg.format(date) + "-" + Action;
 		System.out.println(msg);
-		
-		FileWriter fw = new FileWriter("C:\\AnaManager\\Analytics.txt", true);
-		PrintWriter out = new PrintWriter(fw);
-		out.println(msg);
-		out.close();
-		fw.close();
+		try{
+			String workingDirectory = System.getProperty("user.home");
+			String absoluteFilePath = workingDirectory + File.separator + logFileName;
+			
+			System.out.println("Final filepath : " + absoluteFilePath);
+			
+			dirFile = new File(absoluteFilePath);
+			
+			if (dirFile.createNewFile()) {
+				System.out.println("File is created!");
+			} else {
+				System.out.println("File is already existed!");
+			}
+
+		  } catch (IOException e) {
+			e.printStackTrace();
+		  }
+		if(dirFile != null){	
+			FileWriter fw = new FileWriter(dirFile, true);
+			PrintWriter out = new PrintWriter(fw);
+			out.println(msg);
+			out.close();
+			fw.close();
+		}else{
+			System.out.println("Error: directory file is null. No File opened.");
+		}
 	}
 	
 	
-	/**
-	 * clear the log file- useful for anaViewer or emergency data clearing.
-	 * @throws IOException
-	 */
+	//clear the log file- useful for anaViewer or emergency data clearing.
 	public static void clearData() throws IOException{
-		FileWriter fileWriter = new FileWriter("C:\\AnaManager\\Analytics.txt", false);
+		File dirFile = null;
+		try{
+			//options: user.home or user.dir - depending on how I want to use it.
+			String workingDirectory = System.getProperty("user.home");
+			String absoluteFilePath = workingDirectory + File.separator + logFileName;
+			//System.out.println("Final filepath : " + absoluteFilePath);
+			
+			dirFile = new File(absoluteFilePath);
+			
+			if (dirFile.createNewFile()) {
+				System.out.println("File is created!");
+			} else {
+				System.out.println("File is already existed!");
+			}
+
+		  } catch (IOException e) {
+			e.printStackTrace();
+		  }
+		
+		
+		if(dirFile != null){
+		FileWriter fileWriter = new FileWriter(dirFile, false);
 		fileWriter.write("");
 		fileWriter.close();
-		
+
 		System.out.println("Data Cleared");
+		}else{
+			System.out.println("Unable to open log file");
+		}
 	}
 }
 
